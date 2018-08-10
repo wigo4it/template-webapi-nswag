758f54d8212b31e44f271ce503f8ff616f42df77
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2018-08-10 22:12:52 +0000

    Fix template build

--- build-template.bat
+++ build-template.bat
@@ -1,3 +1,3 @@
-dotnet new -u Wigo4It.WebApiNSwagEfTemplate.CSharp
-nuget pack Wigo4It.WebApiNSwagEfTemplate.CSharp.nuspec
-dotnet new -i "Wigo4It.WebApiNSwagEfTemplate.CSharp.0.0.1.nupkg"
+dotnet new -u WebApiNSwagEfTemplate
+nuget pack WebApiNSwagEfTemplate.nuspec
+dotnet new -i WebApiNSwagEfTemplate.0.0.1.nupkg

454ffb67f580a97d89a867a92e0f9c8d679b9fda
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2018-08-10 22:01:14 +0000

    Code cleanup

--- content/template-identifier/Controllers/SampleEfController.cs
+++ content/template-identifier/Controllers/SampleEfController.cs
@@ -1,13 +1,7 @@
-using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using template_identifier.Models;
-using System.Net;
-using System.Web.Http;
-using Microsoft.AspNet.OData;
 using static template_identifier.Models.SampleEfModel;
 using AutoMapper;
 using static template_identifier.Models.DTO.SampleModelDTO;

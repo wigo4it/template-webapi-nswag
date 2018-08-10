146a0e49c25d628040eeb3b8787f2b086d68681a
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2018-08-10 10:13:15 +0000

    Adding parameter model for db paging

---
+++ content/template-identifier/Models/Common/PagingParametersModel.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace template_identifier.Common.Models
+{
+    public class PagingParameterModel  
+    {  
+        public int MaxPageSize { get; set; } = 20;  
+  
+        public int PageNumber { get; set; } = 1;  
+  
+        private int _pageSize { get; set; } = 10;  
+  
+        public int pageSize  
+        {  
+  
+            get { return _pageSize; }  
+            set  
+            {  
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;  
+            }  
+        }  
+    }
+}

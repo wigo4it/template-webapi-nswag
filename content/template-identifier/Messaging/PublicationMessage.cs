b9c8c9c316dc4a7b31c22dfd1fd38cd19d1eec85
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2018-08-12 08:00:24 +0000

    adding publication message type

--- content/template-identifier/Messaging/PublicationMessage.cs
+++ content/template-identifier/Messaging/PublicationMessage.cs
@@ -1,2 +1,20 @@
+using System;
+
 namespace template_identifier.PublicationMessage {
+    public class PublicationMessage {
+        public string Topic {get;set;}
+        public string UserData {get;set;}
+        public bool IsRetained {get;set;}
+        public PublicationOptions Options {get;set;}
+        public uint Level {get;set;}
+        public DateTime Date {get;set;}
+        public uint Sequence {get;set;}
+        public string Data {get;set;}
+    }
+
+    public enum PublicationOptions
+    {
+        Default = 0
+    }
+
 }
